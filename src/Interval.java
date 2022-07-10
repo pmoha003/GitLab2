a57225b7ada0c59aec60a24882b2227345c2383b
PREVIA MOHAN <pmoha003@odu.edu>
2022-07-10 16:04:39 -0400

    commit

--- src/Interval.java
+++ src/Interval.java
@@ -1,6 +1,6 @@
-public class App {
+public class Interval {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
     }
 }
-//This from master 
+//This is from testBranch
